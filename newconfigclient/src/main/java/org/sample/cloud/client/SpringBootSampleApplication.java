7f5c2559cb519e9345781dc9189e11a8cade7e9a
wangzigui <18278428063@163.com>
2017-08-12 18:48:02 +0800

    modify

--- newconfigclient/src/main/java/org/sample/cloud/client/SpringBootSampleApplication.java
+++ newconfigclient/src/main/java/org/sample/cloud/client/SpringBootSampleApplication.java
@@ -4,15 +4,13 @@ package org.sample.cloud.client;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.embedded.ConfigurableEmbeddedServletContainer;
-import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
 
 @EnableDiscoveryClient
 @SpringBootApplication
 @EnableFeignClients
-public class SpringBootSampleApplication  implements EmbeddedServletContainerCustomizer{
+public class SpringBootSampleApplication{
 
 	@Value("${server.port}")
 	private int port;
@@ -21,9 +19,6 @@ public class SpringBootSampleApplication  implements EmbeddedServletContainerCus
 		SpringApplication.run(SpringBootSampleApplication.class, args);
 	}
 
-	@Override
-	public void customize(ConfigurableEmbeddedServletContainer container) {
-		container.setPort(8004); 
-	}
+
 	
 }
--- newconfigclient1/src/main/java/org/sample/cloud/client/SpringBootSampleApplication.java
+++ newconfigclient1/src/main/java/org/sample/cloud/client/SpringBootSampleApplication.java
@@ -3,20 +3,15 @@ package org.sample.cloud.client;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.embedded.ConfigurableEmbeddedServletContainer;
-import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @EnableDiscoveryClient
 @SpringBootApplication
-public class SpringBootSampleApplication implements EmbeddedServletContainerCustomizer{
+public class SpringBootSampleApplication{
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootSampleApplication.class, args);
 	}
 	
-	public void customize(ConfigurableEmbeddedServletContainer configurableEmbeddedServletContainer) {  
-  
-        configurableEmbeddedServletContainer.setPort(8005);  
-    }  
+	
 }
